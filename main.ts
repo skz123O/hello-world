8e7c8d4c11747ded546503f9dd54b6b82af03dc3
skz123O <122539715+skz123O@users.noreply.github.com>
2023-01-13 08:59:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Happy)
 })
 basic.showIcon(IconNames.Heart)
